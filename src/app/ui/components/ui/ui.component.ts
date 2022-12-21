a5f45ca986a10138228b6588dcd1df1497f43583
sophiemariepauline <34586487+sophiemariepauline@users.noreply.github.com>
2022-12-21 14:55:19 +0100

    add ui

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent {
+  public close: boolean;
 
+  constructor() {
+    this.close = false;
+  }
+
+  public toggle(): void {
+    this.close = !this.close;
+  }
 }
