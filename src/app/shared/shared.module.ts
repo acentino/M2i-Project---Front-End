29c3521842841dc0d13635b3e927ecb806bc9270
sophiemariepauline <34586487+sophiemariepauline@users.noreply.github.com>
2022-12-21 16:54:59 +0100

    add channels list in nav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IconsModule } from '../icons/icons.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule],
+  exports: [TemplatesModule, IconsModule],
 })
-export class SharedModule { }
+export class SharedModule {}
