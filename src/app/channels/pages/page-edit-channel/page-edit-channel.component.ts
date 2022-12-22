9a8aef09eea76e3e099a4161917a459fd4daf064
acentino <42096872+acentino@users.noreply.github.com>
2022-12-22 16:37:55 +0100

    add edit channel

--- src/app/channels/pages/page-edit-channel/page-edit-channel.component.ts
+++ src/app/channels/pages/page-edit-channel/page-edit-channel.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Channel } from 'src/app/core/models/channel';
+import { ChannelsService } from 'src/app/core/services/channels.service';
 
 @Component({
   selector: 'app-page-edit-channel',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-edit-channel.component.scss']
 })
 export class PageEditChannelComponent {
+  public channel : Channel;
 
+  constructor(private channelsService : ChannelsService, private router : Router){
+    this.channel= new Channel();
+  }
+
+  public action(channel: Channel) {
+    channel.user = {
+      id: 1,
+      userName: "User",
+      email: "user@baggy.com"
+    }
+    console.log();
+
+      this.channelsService.edit(channel).subscribe(() => {
+      this.router.navigate(['channels']);
+    });
+
+  }
 }
