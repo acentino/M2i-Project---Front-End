88b47c5fe2356f4b9594fdc6bae5c19c9ac62c85
acentino <42096872+acentino@users.noreply.github.com>
2022-12-22 12:46:51 +0100

    Update template-full-width.component.ts

--- src/app/templates/components/template-full-width/template-full-width.component.ts
+++ src/app/templates/components/template-full-width/template-full-width.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template-full-width',
   templateUrl: './template-full-width.component.html',
-  styleUrls: ['./template-full-width.component.scss']
+  styleUrls: ['./template-full-width.component.scss'],
 })
 export class TemplateFullWidthComponent {
   @Input() public title: string;
