3a6af6042a83ff3ddd6e30711773417e362ac20e
FlorianBonhoure <florian.b47@hotmail.fr>
2022-12-22 09:43:13 +0100

    add user service

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,14 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environment/environment';
+import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  public urlApi: string;
+  public collection$: Observable<User[]>;
 
   constructor(private httpClient: HttpClient) {
-    public urlApi: string;
-    public collection$: Observable<User>
-   }
+    this.urlApi = environment.urlApi;
+    this.collection$ = this.httpClient.get<User[]>(`${this.urlApi}/users`);
+  }
 }
