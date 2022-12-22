9331c52ab3fc457680a0e14bf92741c1aac5d0fb
FlorianBonhoure <florian.b47@hotmail.fr>
2022-12-22 14:53:18 +0100

    add list messages by channel

--- src/app/core/services/messages.service.ts
+++ src/app/core/services/messages.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { environment } from 'src/environment/environment';
 import { Message } from '../models/message';
 
@@ -9,10 +9,24 @@ import { Message } from '../models/message';
 })
 export class MessagesService {
   public urlApi: string;
-  public collection$!: Observable<Message[]>;
+  public channelMessages$ : BehaviorSubject<Message[]>;
+  public channelName: BehaviorSubject<string>;
+  public channelId: BehaviorSubject<number>;
 
   constructor(private httpClient: HttpClient) {
     this.urlApi = environment.urlApi;
-    this.collection$ = this.httpClient.get<Message[]>(`${this.urlApi}/messages`);
+    this.channelMessages$ = new BehaviorSubject<Message[]>([]);
+    this.channelName = new BehaviorSubject<string>("");
+    this.channelId = new BehaviorSubject<number>(0);
+    this.channelMessages$.subscribe();
    }
+
+   public getMessagesByChannel(channelId: number) {
+    this.httpClient.get<Message[]>(`${this.urlApi}/messages/${channelId}`).subscribe((data) => {
+      this.channelMessages$.next(data);
+    })
+
+
+   }
+
 }
--- src/app/messages/pages/page-list-messages/page-list-messages.component.ts
+++ src/app/messages/pages/page-list-messages/page-list-messages.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Message } from 'src/app/core/models/message';
+import { MessagesService } from 'src/app/core/services/messages.service';
 
 @Component({
   selector: 'app-page-list-messages',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-list-messages.component.scss']
 })
 export class PageListMessagesComponent {
+  public channelMessages$!: BehaviorSubject<Message[]>;
+  public channelName!: string;
+  public channelId!: number;
+
+
+  constructor(private messagesService: MessagesService){
+    this.channelMessages$ = this.messagesService.channelMessages$;
+    messagesService.channelName.subscribe( (data) => this.channelName = data);
+    messagesService.channelId.subscribe( (data) => this.channelId = data);
+  }
 
 }
